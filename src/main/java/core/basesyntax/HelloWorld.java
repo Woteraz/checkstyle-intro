c9ccd2d790fea71dce274589b36d1721ff3584f8
Vasyl Dykun <lotusar123@gmail.com>
2025-09-20 22:24:30 +0300

    Fixed line 10,the name of variable was not correct. Changed from A to greeting.

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 
 public class HelloWorld {
 
-    private String a;
+    private String greeting;
     private int variable;
     private int age, size;
 
@@ -33,8 +33,8 @@ public class HelloWorld {
     }
 
     private void initializeVariables() {
-        a = "Hello mates!";
-        System.out.println(a);
+        greeting = "Hello mates!";
+        System.out.println(greeting);
         variable = LocalDate.now().getYear();
         System.out.println("It is " + variable + "year");
         age = 25;
