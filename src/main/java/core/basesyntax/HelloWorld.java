6800deef9fa029969b1e1e635037ea7914d45ab8
Vasyl Dykun <lotusar123@gmail.com>
2025-09-20 22:34:57 +0300

    Fixed line 17, moved brace to line 18

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -14,7 +14,8 @@ public class HelloWorld {
    
     public static void main(String[] args) {
         Dog dog = new Dog();
-        Cat cat = new Cat(); }
+        Cat cat = new Cat(); 
+    }
 
     private String getFromOneToTen() {
         String result="";
