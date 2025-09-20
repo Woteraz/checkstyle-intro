049df269dc11f7e8ce89416d71028a7991f19a02
Vasyl Dykun <lotusar123@gmail.com>
2025-09-20 22:14:24 +0300

    Fixed 10:20 (variable)

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 public class HelloWorld {
 
     private String a;
-    private int vARIaBLE;
+    private int variable;
     private int age, size;
 
 
@@ -35,8 +35,8 @@ public class HelloWorld {
     private void initializeVariables() {
         a = "Hello mates!";
         System.out.println(a);
-        vARIaBLE = LocalDate.now().getYear();
-        System.out.println("It is " + vARIaBLE + "year");
+        variable = LocalDate.now().getYear();
+        System.out.println("It is " + variable + "year");
         age = 25;
         System.out.println("I'm " + age);
         size = 42;
